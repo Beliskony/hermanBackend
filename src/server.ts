4aa82ae75b634793a470ba5cc8ed57a9b5658545
Axel NGuessan <nguessanaxel21@gmail.com>
2026-01-16 23:49:28 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,12 +12,13 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [ "http://localhost:5173", "https://expertise-two.vercel.app/" ],
+  origin: [ "http://localhost:5173", "https://expertise-two.vercel.app" ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
-
+app.options("*", cors());
 app.use(express.json());
 
 
