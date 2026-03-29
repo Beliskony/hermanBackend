6aeae3e9a5dadaf2a3417f7b96f734fbb9fc9ef4
Axel NGuessan <axelsylvain9@gmail.com>
2026-03-29 13:24:39 +0000

    finalisation et vrai domaine

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [ "http://localhost:5173", "https://expertise-two.vercel.app", "https://herman-admin-qhgr.vercel.app" ],
+  origin: [ "http://localhost:5173", "https://admin.acenviro.pro", "https://acenviro.pro" ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
